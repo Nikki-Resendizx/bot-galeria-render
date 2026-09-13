84f79047aa6df6de3bb203f787e61c8bfeb6cf40
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-13 01:46:45 -0600

    Load admin handler in index.js

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,5 +1,10 @@
+// Este archivo importa TODOS los módulos para que el bot los cargue
+// Orden importa: primero middlewares, luego handlers
+
 require('./start');
-require('./admin');
 require('./galeria');
 require('./fotos');
 require('./text');
+require('./admin');
+
+console.log('✅ V14 Handlers cargados: start, galeria, fotos, text, admin + premium middleware');
