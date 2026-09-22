86df4d317295d36828d8c2067729fd4fc7513198
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 01:48:20 -0600

    Simplify BOT_TOKEN check and add error logging
    Refactor error handling and improve bot initialization.

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,13 @@
 const { Telegraf } = require('telegraf');
 const { BOT_TOKEN } = require('./config');
 
-if (!BOT_TOKEN) {
-  throw new Error('BOT_TOKEN no definido en Render > Environment');
-}
+if (!BOT_TOKEN) throw new Error("BOT_TOKEN no definido en .env");
 
 const bot = new Telegraf(BOT_TOKEN);
 
-// Middleware premium (por ahora deja pasar todo)
-const premiumMiddleware = require('./middlewares/premium');
-bot.use(premiumMiddleware);
-
-// Cargar handlers con inyección de bot
-require('./handlers')(bot);
+// Para que no se caiga todo el deploy con un error
+bot.catch((err, ctx) => {
+  console.log(`Unhandled error while processing ${ctx.updateType}`, err);
+});
 
 module.exports = { bot };
