3232a716b4303597fe2ac27d539f4bb93401fe57
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 04:41:19 -0600

    Delete src/firebase.js

--- src/firebase.js
+++
@@ -1,7 +0,0 @@
-const { initializeApp, getApps, getApp } = require('firebase/app');
-const { getFirestore } = require('firebase/firestore');
-const { FIREBASE } = require('./config');
-let app = getApps().length ? getApp() : initializeApp(FIREBASE);
-const db = getFirestore(app);
-console.log("🔥 Firebase V16");
-module.exports = { db, app };
