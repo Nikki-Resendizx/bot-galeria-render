4697085115e5bafdae2add4f87d6f64f7386b2f3
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 06:13:44 -0600

    Add single model lookup for template rendering

--- src/config/db.js
+++ src/config/db.js
@@ -9,8 +9,8 @@ async function saveUser(id,info){await db.collection('usuarios').doc(String(id))
 async function getPlantillas(){const s=await db.collection('plantillas').get(),o={};s.forEach(d=>o[d.id]=Object.assign({id:d.id},d.data()));return o;}
 async function savePlantilla(id,data){await db.collection('plantillas').doc(id).set(data,{merge:true});}
 async function deletePlantilla(id){await db.collection('plantillas').doc(id).delete();}
-async function getModelos(){const s=await db.collection('modelos').get();return s.docs.map(d=>Object.assign({id:d.id},d.data()));}
+async function getModelo(id){const s=await db.collection('modelos').doc(String(id)).get();return s.exists?Object.assign({id:s.id},s.data()):null;}\nasync function getModelos(){const s=await db.collection('modelos').get();return s.docs.map(d=>Object.assign({id:d.id},d.data()));}
 async function getBotMedia(){const s=await botDoc.get();return s.exists?((s.data()||{}).media||{}):{};}
 async function saveBotMedia(key,fileId){await botDoc.set({media:{[key]:fileId}},{merge:true});}
 async function saveTemplateMedia(id,fileId){await db.collection('plantillas').doc(String(id)).set({media_file_id:fileId,actualizado:admin.firestore.FieldValue.serverTimestamp()},{merge:true});}
-module.exports={db,getConfig,saveConfig,saveUser,getPlantillas,savePlantilla,deletePlantilla,getModelos,getBotMedia,saveBotMedia,saveTemplateMedia};
+module.exports={db,getConfig,saveConfig,saveUser,getPlantillas,savePlantilla,deletePlantilla,getModelo,getModelos,getBotMedia,saveBotMedia,saveTemplateMedia};
