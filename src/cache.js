34287f1e01b87544599d4cb05309d00103bf452c
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 11:19:05 -0600

    fix: sincronizar cache con cambios de configuracion

--- src/cache.js
+++ src/cache.js
@@ -1,3 +1,12 @@
-const { getConfig }=require('./config/db'); let value=null,time=0;
-async function getCachedConfig(){if(value&&Date.now()-time<60000)return value;value=await getConfig();time=Date.now();return value;}
-function clearCache(){value=null;time=0;} module.exports={getConfig:getCachedConfig,clearCache};
+const { getConfig }=require('./config/db');
+let value=null,time=0,version=0;
+async function getCachedConfig(){
+  const currentVersion = Number(global.__verifiedmodelsConfigVersion || 0);
+  if(value && Date.now()-time<60000 && version===currentVersion) return value;
+  value=await getConfig();
+  time=Date.now();
+  version=currentVersion;
+  return value;
+}
+function clearCache(){value=null;time=0;version=0;}
+module.exports={getConfig:getCachedConfig,clearCache};
