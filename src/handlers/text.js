0840dc924d3fc2084a08f2600a4b980109a79fee
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 06:13:20 -0600

    Refactor bot to Firebase and Telegram media storage

--- src/handlers/text.js
+++ src/handlers/text.js
@@ -1,8 +1,1 @@
-module.exports = (bot) => {
-  bot.on('text', (ctx, next) => {
-    const t = ctx.message.text;
-    if(t.startsWith('/')) return next();
-    // Si no es comando, no hace nada para no interferir
-    return next();
-  });
-};
+module.exports=bot=>{bot.on('text',(ctx,next)=>next());};
