a1b0192f8d1e76e50ae8964828ee005f02412567
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 00:29:24 -0600

    Refactor bot.js to simplify handler imports

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,8 @@
-// src/bot.js
 const { Telegraf } = require('telegraf');
 const { BOT_TOKEN } = require('./config');
-
 if (!BOT_TOKEN) throw new Error('BOT_TOKEN no definido en Render');
-
 const bot = new Telegraf(BOT_TOKEN);
-
-// 1. Middleware premium PRIMERO
 const premiumMiddleware = require('./middlewares/premium');
 bot.use(premiumMiddleware);
-
-// 2. Luego los handlers
-require('./handlers/index')(bot);
-
+require('./handlers')(bot);
 module.exports = { bot };
