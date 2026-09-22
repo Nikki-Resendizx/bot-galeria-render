a098fa1a4ac737d1589a36f2ce6d01c7697bd0c9
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 01:04:00 -0600

    Delete src directory

--- src/bot.js
+++
@@ -1,8 +0,0 @@
-const { Telegraf } = require('telegraf');
-const { BOT_TOKEN } = require('./config');
-if (!BOT_TOKEN) throw new Error('BOT_TOKEN no definido en Render');
-const bot = new Telegraf(BOT_TOKEN);
-const premiumMiddleware = require('./middlewares/premium');
-bot.use(premiumMiddleware);
-require('./handlers')(bot);
-module.exports = { bot };
--- src/handlers/index.js
+++
@@ -1,7 +0,0 @@
-// src/handlers/index.js - CORRECTO v14
-module.exports = (bot) => {
-  require('./text')(bot);
-  require('./galeria')(bot);
-  require('./admin')(bot);
-  console.log('✅ Handlers cargados: text, galeria, admin');
-};
