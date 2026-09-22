e7fd3dbf7dbbb667eb093db335d80ed0eea1f23c
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-21 23:08:39 -0600

    Refactor bot.js to use config for BOT_TOKEN

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,16 @@
+// src/bot.js
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const { BOT_TOKEN } = require('./config');
+
+if (!BOT_TOKEN) throw new Error('BOT_TOKEN no definido en Render');
+
+const bot = new Telegraf(BOT_TOKEN);
+
+// 1. Middleware premium PRIMERO
 const premiumMiddleware = require('./middlewares/premium');
-premiumMiddleware(bot);
-module.exports={ bot };
+bot.use(premiumMiddleware);
+
+// 2. Luego los handlers
+require('./handlers/index')(bot);
+
+module.exports = { bot };
