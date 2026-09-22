bada65cbcb1f852f2ad69be264613833f8d786f5
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 07:52:08 -0600

    feat: botones de inicio con color y emoji premium

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,2 +1,14 @@
-const { saveUser,getConfig,getBotMedia }=require('../config/db');const { replaceVars }=require('../utils');const { Markup }=require('telegraf');
-module.exports=bot=>bot.start(async ctx=>{await saveUser(ctx.from.id,{username:ctx.from.username||'',first_name:ctx.from.first_name||'',last_name:ctx.from.last_name||''});const c=await getConfig(),m=await getBotMedia(),t=replaceVars(c.bienvenida_texto||'👋 ¡Hola {mencion}! 💎\n\nBienvenid@ a VerifiedModels ✨',ctx),kb=Markup.inlineKeyboard([[Markup.button.webApp('💎 Galería Virtual 💎',process.env.WEBAPP_URL||'')],[Markup.button.callback('👑 Lista de Modelos 👑','public_modelos')],...(process.env.CANAL_FREE_URL?[Markup.button.url('📢 Canal FREE 📢',process.env.CANAL_FREE_URL)]:[])]);if(m.bienvenida)return ctx.replyWithPhoto(m.bienvenida,{caption:t,parse_mode:'HTML',...kb});return ctx.reply(t,{parse_mode:'HTML',...kb});});
+const { saveUser,getConfig,getBotMedia }=require('../config/db');
+const { replaceVars }=require('../utils');
+const { Markup }=require('telegraf');
+const { webAppButton,urlButton,button }=require('../buttons');
+
+module.exports=bot=>bot.start(async ctx=>{
+  await saveUser(ctx.from.id,{username:ctx.from.username||'',first_name:ctx.from.first_name||'',last_name:ctx.from.last_name||''});
+  const c=await getConfig(),m=await getBotMedia(),t=replaceVars(c.bienvenida_texto||'👋 ¡Hola {mencion}! 💎\n\nBienvenid@ a VerifiedModels ✨',ctx);
+  const rows=[[await webAppButton('webapp',process.env.WEBAPP_URL||''),await button('modelos')]];
+  if(process.env.CANAL_FREE_URL)rows.push([await urlButton('canal_free',process.env.CANAL_FREE_URL)]);
+  const kb=Markup.inlineKeyboard(rows);
+  if(m.bienvenida)return ctx.replyWithPhoto(m.bienvenida,{caption:t,parse_mode:'HTML',...kb});
+  return ctx.reply(t,{parse_mode:'HTML',...kb});
+});
