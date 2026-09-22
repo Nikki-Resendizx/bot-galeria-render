5ff2b9d497c44638af34204b232ebe4bd3571040
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 07:52:12 -0600

    feat: botón WebApp con estilo configurable

--- src/handlers/galeria.js
+++ src/handlers/galeria.js
@@ -1,1 +1,9 @@
-const { getBotMedia }=require('../config/db');const { Markup }=require('telegraf');module.exports=bot=>bot.command('galeria',async ctx=>{const m=await getBotMedia(),kb=Markup.inlineKeyboard([[Markup.button.webApp('💎 Abrir Galería 💎',process.env.WEBAPP_URL||'')]]);if(m.galeria)return ctx.replyWithPhoto(m.galeria,{caption:'🖼️ <b>GALERÍA VIRTUAL</b> 💎',parse_mode:'HTML',...kb});return ctx.reply('🖼️ <b>GALERÍA VIRTUAL</b> 💎',{parse_mode:'HTML',...kb});});
+const { getBotMedia }=require('../config/db');
+const { Markup }=require('telegraf');
+const { webAppButton }=require('../buttons');
+module.exports=bot=>bot.command('galeria',async ctx=>{
+  const m=await getBotMedia();
+  const kb=Markup.inlineKeyboard([[await webAppButton('webapp',process.env.WEBAPP_URL||'')]]);
+  if(m.galeria)return ctx.replyWithPhoto(m.galeria,{caption:'🖼️ <b>GALERÍA VIRTUAL</b> 💎',parse_mode:'HTML',...kb});
+  return ctx.reply('🖼️ <b>GALERÍA VIRTUAL</b> 💎',{parse_mode:'HTML',...kb});
+});
