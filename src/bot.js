d15cbb65f8e96a8a72c3ad880c7b33e9987086d3
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 03:28:58 -0600

    Refactor bot.js for cleaner error handling
    Simplified error handling and registration of handlers.

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,7 @@
 const { Telegraf } = require('telegraf');
 const { BOT_TOKEN } = require('./config');
-
-if (!BOT_TOKEN) throw new Error("BOT_TOKEN no definido en .env");
-
+if(!BOT_TOKEN) throw new Error("BOT_TOKEN");
 const bot = new Telegraf(BOT_TOKEN);
-
-// CARGAMOS LOS HANDLERS
-const registerHandlers = require('./handlers');
-registerHandlers(bot);
-
-bot.catch((err, ctx) => {
-  console.log(`Unhandled error while processing ${ctx.updateType}`, err);
-});
-
+require('./handlers')(bot);
+bot.catch((err,ctx)=> console.log(`Error ${ctx.updateType}`,err.message));
 module.exports = { bot };
