cfd2ae0adf2cea2baa762512edc236d072415cd3
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-22 06:13:00 -0600

    Refactor bot to Firebase and Telegram media storage

--- src/config/db.js
+++ src/config/db.js
@@ -1,54 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-const DB_PATH = '/tmp/database.json';
-
-function loadDB(){
-  try{
-    if(!fs.existsSync(DB_PATH)){
-      return { users:{}, modelos:[], plantillas:{}, bienvenida:'', botones:[], galeria:[] };
-    }
-    return JSON.parse(fs.readFileSync(DB_PATH,'utf8'));
-  }catch(e){
-    return { users:{}, modelos:[], plantillas:{}, bienvenida:'', botones:[], galeria:[] };
-  }
+const { db, admin } = require('../firebase');
+const botDoc = db.collection('config').doc('bot');
+async function getConfig() {
+  const a=await botDoc.get(), b=await db.collection('config').doc('botones').get(), p=await db.collection('config').doc('premium').get();
+  return Object.assign({},a.exists?a.data():{},{botones:b.exists?b.data():{},premium:p.exists?p.data():{activo:true}});
 }
-function saveDB(data){
-  try{ fs.writeFileSync(DB_PATH, JSON.stringify(data,null,2)); }catch(e){}
-}
-
-// USERS
-function saveUser(id, info){ const db=loadDB(); db.users[id]=info; saveDB(db); }
-function getUsers(){ return loadDB().users; }
-
-// BIENVENIDA
-function saveBienvenida(texto){ const db=loadDB(); db.bienvenida=texto; saveDB(db); }
-function getBienvenida(){ return loadDB().bienvenida; }
-
-// PLANTILLAS
-function savePlantilla(nombre, texto){ const db=loadDB(); db.plantillas[nombre]=texto; saveDB(db); }
-function getPlantillas(){ return loadDB().plantillas; }
-function getPlantilla(nombre){ return loadDB().plantillas[nombre]; }
-
-// MODELOS (foto, nombre, descripcion, etc)
-function saveModelos(modelos){ const db=loadDB(); db.modelos=modelos; saveDB(db); }
-function getModelos(){ return loadDB().modelos || []; }
-function addModelo(m){ const db=loadDB(); db.modelos.push(m); saveDB(db); }
-function deleteModelo(id){ const db=loadDB(); db.modelos=db.modelos.filter(x=>x.id!=id); saveDB(db); }
-
-// GALERIA
-function getGaleria(){ return loadDB().galeria || []; }
-function saveGaleria(g){ const db=loadDB(); db.galeria=g; saveDB(db); }
-
-// BOTONES PREMIUM
-function getBotones(){ return loadDB().botones || []; }
-function saveBotones(b){ const db=loadDB(); db.botones=b; saveDB(db); }
-
-module.exports = {
-  loadDB, saveDB,
-  saveUser, getUsers,
-  saveBienvenida, getBienvenida,
-  savePlantilla, getPlantillas, getPlantilla,
-  saveModelos, getModelos, addModelo, deleteModelo,
-  getGaleria, saveGaleria,
-  getBotones, saveBotones
-};
+async function saveConfig(data){await botDoc.set(data,{merge:true});}
+async function saveUser(id,info){await db.collection('usuarios').doc(String(id)).set(Object.assign({},info,{id:String(id),actualizado:admin.firestore.FieldValue.serverTimestamp()}),{merge:true});}
+async function getPlantillas(){const s=await db.collection('plantillas').get(),o={};s.forEach(d=>o[d.id]=Object.assign({id:d.id},d.data()));return o;}
+async function savePlantilla(id,data){await db.collection('plantillas').doc(id).set(data,{merge:true});}
+async function deletePlantilla(id){await db.collection('plantillas').doc(id).delete();}
+async function getModelos(){const s=await db.collection('modelos').get();return s.docs.map(d=>Object.assign({id:d.id},d.data()));}
+async function getBotMedia(){const s=await botDoc.get();return s.exists?((s.data()||{}).media||{}):{};}
+async function saveBotMedia(key,fileId){await botDoc.set({media:{[key]:fileId}},{merge:true});}
+async function saveTemplateMedia(id,fileId){await db.collection('plantillas').doc(String(id)).set({media_file_id:fileId,actualizado:admin.firestore.FieldValue.serverTimestamp()},{merge:true});}
+module.exports={db,getConfig,saveConfig,saveUser,getPlantillas,savePlantilla,deletePlantilla,getModelos,getBotMedia,saveBotMedia,saveTemplateMedia};
